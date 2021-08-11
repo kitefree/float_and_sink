f56dc8810e75eb1674104f954c2ec26857fe8645
DESKTOP-22DDDV5\RESH <kitefree@gmail.com>
2021-08-11 09:42:55 +0800

    add judge tablet

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 import MainScene from './Scenes/MainScene.js';
 
+function isIPadDevice() {
+    if (/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+gWidth = 0;
 
+if (isIPadDevice() == true)
+{
+    gWidth = 1200;
+}
+else {
+    gWidth = 1500;
+}
 
 var config = {
     type: Phaser.WEBGL,
-    width: 1500,
+    width: gWidth,
     height: 700,
     backgroundColor: '#2d2d2d',
     //transparent: true, // 背景透明
